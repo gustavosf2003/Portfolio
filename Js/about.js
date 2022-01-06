5c0d1df2810e523b92bdc21cd06c1a97f9da3ee6
Gustavo Ferreira <gustavo.ferreira@orbital.company>
2022-01-06 18:09:31 -0300

    Fix: Projects responsiveness

--- Js/about.js
+++ Js/about.js
@@ -1,6 +1,6 @@
 function smile(){
     document.getElementById("img").src="Img/avatar_smile.png"
 }
-// function easterEgg(){
-//     alert("Click on the avatar")
-// }
+function normalFace(){
+    document.getElementById("img").src="Img/avatar.png"
+}
